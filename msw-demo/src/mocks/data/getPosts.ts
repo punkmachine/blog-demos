c0d3171afe91cd9619ff991d326c9f59c57b9967
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-09-12 12:15:23 +0500

    save msw demo

--- msw-demo/src/mocks/data/getPosts.ts
+++ msw-demo/src/mocks/data/getPosts.ts
@@ -1,12 +1,12 @@
 import { faker } from '@faker-js/faker';
 import type { IPost } from '@/types/Posts';
 
-export const getPosts = (limit: number = 15): IPost[] => {
+export const getPosts = (limit: number = 15, userId?: number | null): IPost[] => {
   const mockData = [];
 
   for (let i = 1; i <= limit; i++) {
     mockData.push({
-      userId: faker.number.int({ min: 1, max: 100 }),
+      userId: userId ?? faker.number.int({ min: 1, max: 100 }),
       id: i,
       title: faker.lorem.words(),
       body: faker.lorem.paragraph(),
--- msw-demo/src/mocks/handlers/getPosts.ts
+++ msw-demo/src/mocks/handlers/getPosts.ts
@@ -1,11 +1,15 @@
 import { HttpResponse } from 'msw';
 import { getPosts } from '../data/getPosts';
 
+const getUserId = (): number | null => {
+  return localStorage.getItem('userId') ? Number(localStorage.getItem('userId')) : null;
+}
+
 export function getPostsHandler({ request }) {
   const searchParams = new URLSearchParams(new URL(request.url).search);
   const limit = Number(searchParams.get('limit'));
 
-  return HttpResponse.json(getPosts(limit), {
+  return HttpResponse.json(getPosts(limit, getUserId()), {
     status: 200,
   });
 }
